8cd1fb3a0594e3b25dbe7b68f80fa5aba69baa6e
Mariugo <mariugomegiani@gmail.com>
2021-04-26 15:12:55 -0300

    Receitas e navegação

--- lib/screens/categorias_receitas_screen.dart
+++ lib/screens/categorias_receitas_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../model/dummy_data.dart';
+
 class CategoriasReceitasScreen extends StatelessWidget {
   final String categoriaId;
   final String categoriaNome;
@@ -11,15 +13,21 @@ class CategoriasReceitasScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final categoriasReceitas = DUMMY_RECEITAS
+        .where((receita) => receita.categorias.contains(categoriaId))
+        .toList();
+
     final myAppBar = AppBar(
       title: Text(categoriaNome),
     );
     return SafeArea(
       child: Scaffold(
         appBar: myAppBar,
-        body: ListView.builder(itemBuilder: (ctx, index) {
-
-        }, itemCount: ,),
+        body: ListView.builder(
+          itemBuilder: (ctx, index) =>
+              Text(categoriasReceitas[index].nomeReceita),
+          itemCount: categoriasReceitas.length,
+        ),
       ),
     );
   }
